8eb4fe23f312baf5c27d60afa8ec5268f899ed08
Andrea Bianchi <andrea@kaist.ac.kr>
2020-08-28 09:45:14 +0900

    Website v1

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,47 @@
 $(function () {
-    
-    
 
     const os = getOS();
-    console.log(os)
-    if (os == 'Mac'){
-        $('#download').text('Download for Mac');
-        $('#otherOs').text('Windows');
-        $('#otherOs').attr('href', 'github.com/makinteractlab/BlinkBoardApp/releases/latest');
 
-    }else if (os == 'Windows'){
-        $('#download').text('Download for Windows');
-        $('#otherOs').text('Mac');
-        $('#otherOs').attr('href', 'github.com/makinteractlab/BlinkBoardApp/releases/latest');
+    // Windows or Mac
+    if (os == 'Mac' || os == 'Windows') {
+        if (os == 'Mac') {
+            $('#download').text('Download for Mac');
+            $('#otherOs').text('Windows');
 
-    }else{
-        $('#download').text('Download');
-        $('#download').click(function(){
-            location.href = "https://github.com/makinteractlab/BlinkBoardApp/releases/latest"
+            getLatestReleaseLink('Mac').then(result => {
+                if (result) $('#download').click(() => location.href = result)
+            });
+            getLatestReleaseLink('Windows').then(result => {
+                if (result) $('#otherOs').attr('href', result);
+            });
+
+        } else { // Windows
+            $('#download').text('Download for Windows');
+            $('#otherOs').text('Mac');
+
+            getLatestReleaseLink('Windows').then(result => {
+                if (result) $('#download').click(() => location.href = result)
+            });
+            getLatestReleaseLink('Mac').then(result => {
+                if (result) $('#otherOs').attr('href', result);
+            });
+        }
+
+    } else {
+        $('#download').click(function () {
+            location.href = "https://github.com/makinteractlab/BlinkBoardApp/releases/latest";
         });
         $('#otherOs').parent().hide();
     }
 });
 
 
-function getLatestMac()
-{
-    const octokit = new Octokit()
+async function getLatestReleaseLink(os) {
+    const result = await octokit.request('GET /repos/{owner}/{repo}/releases/latest', {
+        owner: 'makinteractlab',
+        repo: 'BlinkBoardApp'
+    });
+    return result.data.assets.filter(asset => asset.name == `BlinkBoardApp-${os}.zip`)[0].browser_download_url;
 }
 
 
